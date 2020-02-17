ab9081d1b29f98cc6dde437e3f00ddc317cdb60d
danielmilewski123@gmail.com <danielmilewski123@gmail.com>
2020-02-17 19:28:20 +0100

    Start using PUG

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 // create the new server
 const app = express();
+
+// enable Pug
+app.set('view engine', 'pug');
+
+// add the views folder into the project
+app.set('views', path.join(__dirname, './views') );
+
 
 // listen for the home page
 app.use('/', routes() );
