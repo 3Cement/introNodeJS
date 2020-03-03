59d24d1c1682b735b09db7c083cd288838e2aa55
danielmilewski123@gmail.com <danielmilewski123@gmail.com>
2020-03-03 21:56:49 +0100

    Validation added to the form

--- server/routes/index.js
+++ server/routes/index.js
@@ -45,9 +45,34 @@ module.exports = function() {
         let {name, email, message} = req.body;
 
         // validate the form
+        let errors = [];
 
+        if(!name) {
+            errors.push({'message': 'Add Your Name'})
+        }
 
+        if(!email) {
+            errors.push({'message': 'Add Your Email'})
+        }
+
+        if(!message) {
+            errors.push({'message': 'Add Your Testimonial'})
+        }
+
+        console.log(errors);
         // check if there're some errors
+        if(errors.length > 0) {
+            // we have some errors, display the warning to the view
+            res.render('testimonials', {
+                pageTitle: 'Testimonials',
+                errors ,
+                name,
+                email,
+                message
+            });
+        } else {
+            // save to the database
+        }
 
 
         // save to the database
