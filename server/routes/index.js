26446589a05530f06d58df7850bdb851f7cdbfb3
danielmilewski123@gmail.com <danielmilewski123@gmail.com>
2020-03-04 20:40:29 +0100

    Finish main page

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,12 +8,22 @@ const Testimonials = require('../models/Testimonials');
 module.exports = function() {
     // homepage url
     router.get('/', (req, res) => {
-        Travels.findAll({limit: 3})
-            .then(travels => res.render('index', {
-                pageTitle : 'Home',
-                className: 'home',
-                travels
-            }));
+
+        const promises = [];
+
+        promises.push( Travels.findAll({limit: 3}) );
+        promises.push( Testimonials.findAll({limit: 3}) )
+
+        // pass to the promise
+        const result = Promise.all(promises);
+
+        result.then(result => res.render('index', {
+            pageTitle : 'Home',
+            className: 'home',
+            travels : result[0],
+            testimonials: result[1]
+        }))
+        .catch(err => console.log(err));
     });
 
     // about us
