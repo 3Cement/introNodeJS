beebcc833bea9953927dcb062a56e7d61e758dcb
danielmilewski123@gmail.com <danielmilewski123@gmail.com>
2020-03-04 21:02:27 +0100

    Create controllers for routes

---
+++ server/controllers/travelController.js
@@ -0,0 +1,9 @@
+const Travels = require('../models/Travels');
+
+exports.displayTravels =  (req, res) => {
+    Travels.findAll()
+        .then(travels => res.render('travels', {
+            pageTitle: 'Upcoming Travels',
+            travels
+        }));
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const Travels = require('../models/Travels');
 const Testimonials = require('../models/Testimonials');
 
+// import the controllers
+const aboutController = require('../controllers/aboutController');
+const travelController = require('../controllers/travelController')
+
 module.exports = function() {
     // homepage url
     router.get('/', (req, res) => {
@@ -27,20 +31,8 @@ module.exports = function() {
     });
 
     // about us
-    router.get('/about', (req, res) => {
-        res.render('about', {
-            pageTitle: 'About Us'
-        });
-    });
-
-    router.get('/travels', (req, res) => {
-
-        Travels.findAll()
-            .then(travels => res.render('travels', {
-                pageTitle: 'Upcoming Travels',
-                travels
-            }));
-    });
+    router.get('/about', aboutController.aboutInformation)
+    router.get('/travels', travelController.displayTravels);
 
     router.get('/travels/:id', (req, res) => {
         Travels.findByPk(req.params.id)
